5c9e28a5127c3d703f6a435f077eac5f18119b7f
Yukino Song <nutosservice@gmail.com>
2024-10-21 08:00:00 +0800

    Add jsx-runtime

---
+++ src/renderers/jsx-dev-runtime.js
@@ -0,0 +1,43 @@
+import { nop } from '../utils'
+
+let jsxDEV = nop
+let Fragment = '<>'
+
+const wrap = (R) => {
+	jsxDEV = (tag, {children = [], ...props}, key, ...args) => {
+		try {
+			if (key) {
+				props.key = key
+			}
+			return R.c(tag, props, ...children)
+		} catch (e) {
+			throw new Error(`Error happened while rendering component ${args.join(' ')}`, { cause: e })
+		}
+	}
+	Fragment = R.f
+
+	return {
+		jsxDEV,
+		Fragment
+	}
+}
+
+const _default = {
+	wrap,
+	get default() {
+		return _default;
+	},
+	get jsxDEV() {
+		return jsxDEV;
+	},
+	get Fragment() {
+		return Fragment
+	}
+}
+
+export default _default
+export {
+	wrap,
+	jsxDEV,
+	Fragment
+}
---
+++ src/renderers/jsx-runtime.js
@@ -0,0 +1,51 @@
+import { nop } from '../utils'
+
+let jsx = nop
+let jsxs = nop
+let Fragment = '<>'
+
+const wrap = (R) => {
+	jsx = (tag, {children, ...props}, key) => {
+		if (key) {
+			props.key = key
+		}
+		return R.c(tag, props, children)
+	}
+	jsxs = (tag, {children = [], ...props}, key) => {
+		if (key) {
+			props.key = key
+		}
+		return R.c(tag, props, ...children)
+	}
+	Fragment = R.f
+
+	return {
+		jsx,
+		jsxs,
+		Fragment
+	}
+}
+
+const _default = {
+	wrap,
+	get default() {
+		return _default;
+	},
+	get jsx() {
+		return jsx;
+	},
+	get jsxs() {
+		return jsxs;
+	},
+	get Fragment() {
+		return Fragment
+	}
+}
+
+export default _default
+export {
+	wrap,
+	jsx,
+	jsxs,
+	Fragment
+}
