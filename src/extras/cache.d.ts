86a99296996623a6f5202164b71afb9a6c1e5914
Yukino Song <nutosservice@gmail.com>
2025-10-13 04:23:37 +0800

    Add typescript support

---
+++ src/extras/cache.d.ts
@@ -0,0 +1,36 @@
+/* Copyright Yukino Song, SudoMaker Ltd.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import type { ComponentTemplate } from '../components.js'
+
+export interface CacheStore<T = any> {
+	getIndex(handler: (item: T) => boolean): number
+	add(...items: T[]): void
+	replace(items: T[]): void
+	get(index: number): T | undefined
+	set(index: number, item: T): void
+	del(index: number): void
+	clear(): void
+	size(): number
+	dispose(): void
+	Cached: ComponentTemplate
+}
+
+export function createCache<T = any>(template: ComponentTemplate<T>): CacheStore<T>
